69063648b79768a405e17dafae0db046826a1f53
AdamSzabo1 <98642608+AdamSzabo1@users.noreply.github.com>
2025-01-03 12:54:50 +0100

    fixed a typo in features/auth

--- src/features/auth/index.js
+++ src/features/auth/index.js
@@ -1,4 +1,4 @@
-import LoginForm from "./components/LoginForm/LoginForm";
+import LoginForm from "./components/loginForm/LoginForm";
 import RegisterForm from "./components/registerForm/RegisterForm";
 
 export {
