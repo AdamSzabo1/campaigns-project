f353adbba90845aea0ef5af004311912f6869b7a
AdamSzabo1 <98642608+AdamSzabo1@users.noreply.github.com>
2025-01-10 12:49:04 +0100

    added meta things

--- src/global/routes/router.js
+++ src/global/routes/router.js
@@ -1,10 +1,16 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import DefaultLayout from "../layouts/defaultLayout/DefaultLayout";
-import { AppArea } from "../../shared";
-import { CampaignArticle, CampaignForm, CampaignViewer, loadCampaignArticle, loadCampaignTitle } from "../../features/campaigns";
-import { LoginForm, RegisterForm } from "../../features/auth";
-import { loadUserData, UserProfile } from "../../features/user";
-import { Homepage } from "../../features/static";
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+import DefaultLayout from '../layouts/defaultLayout/DefaultLayout';
+import { AppArea } from '../../shared';
+import {
+    CampaignArticle,
+    CampaignForm,
+    CampaignViewer,
+    loadCampaignArticle,
+    loadCampaignTitle,
+} from '../../features/campaigns';
+import { LoginForm, RegisterForm } from '../../features/auth';
+import { loadUserData, UserProfile } from '../../features/user';
+import { Homepage } from '../../features/static';
 
 const router = createBrowserRouter([
     {
@@ -13,7 +19,7 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Homepage />
+                element: <Homepage />,
             },
             {
                 path: 'auth',
@@ -21,18 +27,18 @@ const router = createBrowserRouter([
                 children: [
                     {
                         path: 'login',
-                        element: <LoginForm />
+                        element: <LoginForm />,
                     },
                     {
                         path: 'register',
-                        element: <RegisterForm />
+                        element: <RegisterForm />,
                     },
                     {
                         path: 'user-profile',
                         element: <UserProfile />,
-                        loader: loadUserData
-                    }
-                ]
+                        loader: loadUserData,
+                    },
+                ],
             },
             {
                 path: 'campaigns',
@@ -40,32 +46,33 @@ const router = createBrowserRouter([
                 children: [
                     {
                         index: true,
-                        element: <h1>Campaigns...</h1>
+                        element: <h1>Campaigns...</h1>,
                     },
                     {
                         path: 'viewer',
                         element: <CampaignViewer />,
                         loader: loadCampaignTitle,
+                        hydrateFallbackElement: <h1>Loading...</h1>,
                         children: [
                             {
                                 index: true,
-                                element: <h1>Please select an article</h1>
+                                element: <h1>Please select an article</h1>,
                             },
                             {
                                 path: ':campaignSlug',
                                 element: <CampaignArticle />,
-                                loader: loadCampaignArticle
-                            }
-                        ]
+                                loader: loadCampaignArticle,
+                            },
+                        ],
                     },
                     {
                         path: 'upload',
-                        element: <CampaignForm />
-                    }
-                ]
+                        element: <CampaignForm />,
+                    },
+                ],
             },
-        ]
-    }
+        ],
+    },
 ]);
 
 export default router;
--- src/shared/layouts/AppArea.jsx
+++ src/shared/layouts/AppArea.jsx
@@ -1,12 +1,11 @@
-import { Outlet } from "react-router";
+import { Outlet } from 'react-router';
 
 const AppArea = () => {
-
     return (
         <div className="container p-4 bg-light my-4">
             <Outlet />
         </div>
     );
-}
+};
 
 export default AppArea;
